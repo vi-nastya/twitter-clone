ba9205e7ec74c8dd2941d01ea390ad9411ee7299
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-08-20 22:43:25 -0700

    Api: return tweet data after successful update

--- api/controllers/tweet.controller.js
+++ api/controllers/tweet.controller.js
@@ -72,7 +72,7 @@ exports.update = (req, res) => {
         res.status(404).send({
           message: `Cannot update Tweet with id=${id}. Maybe Tweet was not found!`,
         })
-      } else res.send({ message: 'Tweet was updated successfully.' })
+      } else res.send(data)
     })
     .catch((err) => {
       res.status(500).send({
