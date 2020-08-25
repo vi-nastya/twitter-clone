55a0b70589f7e51c0279b6e4002fc20c2bc728fe
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-08-24 20:12:06 -0700

    Client: api url

--- client/src/api/api-client.ts
+++ client/src/api/api-client.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-//const baseUrl = 'http://138.68.37.219:9324/api'
-// local setup
-const baseUrl = 'http://localhost:9000/api'
+const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:9000/api'
 
 export const apiClient = axios.create({
   baseURL: baseUrl,
